8519b5aa1ad87208ec341640ea71f597a54cb15e
mabalazs <sha1-814c98c95c0331e45c899668854de808bc93186d@cern.ch>
2007-09-14 13:36:14 +0000

    Code for use of Pseudo Zernike Moments as part of Cluster Shape by Michael A. Balazs. This is preliminary work not intended for release.

--- interface/ClusterShape.h
+++ interface/ClusterShape.h
@@ -8,7 +8,7 @@
  * \author Michael A. Balazs, UVa
  * \author Luca Lista, INFN
  *
- * \version $Id: ClusterShape.h,v 1.5 2006/11/06 14:38:47 mabalazs Exp $
+ * \version $Id: ClusterShape.h,v 1.6 2006/11/13 18:02:05 futyand Exp $
  *
  */
 
@@ -25,9 +25,10 @@ namespace reco {
     ClusterShape() { }
     ClusterShape( double cEE, double cEP, double cPP, 
                   double eMax, DetId eMaxId, double e2nd, DetId e2ndId,
-		  double e2x2, double e3x2, double e3x3, double e4x4,
-		  double e5x5, double E10_Right_, double E10_Left_,
-		  double E10_Top_, double E10_Bottom_, double e3x2Ratio, 
+            		  double e2x2, double e3x2, double e3x3, double e4x4,
+            		  double e5x5, double E10_Right_, double E10_Left_,
+            		  double E10_Top_, double E10_Bottom_, double e3x2Ratio, 
+            		  std::vector<double> pseudoZernikeMoments,
                   std::vector<double> energyBasketFractionEta_,
                   std::vector<double> energyBasketFractionPhi_);
     double eMax() const { return eMax_; }
@@ -45,7 +46,7 @@ namespace reco {
     double covEtaEta() const { return covEtaEta_; }
     double covEtaPhi() const { return covEtaPhi_; }
     double covPhiPhi() const { return covPhiPhi_; }
-
+    double pseudoZernikeMoment(int whichMoment)   const { return pseudoZernikeMoments_[whichMoment]; }
     std::vector<double> energyBasketFractionEta() const { return energyBasketFractionEta_;}
     std::vector<double> energyBasketFractionPhi() const { return energyBasketFractionPhi_;}
     DetId eMaxId() const { return eMaxId_;}
@@ -56,9 +57,10 @@ namespace reco {
     Double32_t eMax_, e2nd_, e2x2_, e3x2_, e3x3_, e4x4_, e5x5_;
     Double32_t e2x5Right_, e2x5Left_, e2x5Top_, e2x5Bottom_;
     Double32_t e3x2Ratio_;
+    DetId eMaxId_, e2ndId_;
+    std::vector<double> pseudoZernikeMoments_; 
     std::vector<double> energyBasketFractionEta_;
     std::vector<double> energyBasketFractionPhi_;
-    DetId eMaxId_, e2ndId_;
   };
 
 }
